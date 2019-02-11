d5fd03ddf7f526c40bf3b8047b680dc7e263d1fd
yafimsva <yafimsva@gmail.com>
2019-02-11 14:49:04 -0500

    added SELECT query

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ try {
 }
 
 
+//INSERT QUERY
+
+
 //Define the query
 $sql = "INSERT INTO pets(type, name, color) VALUES (:type, :name, :color)";
 
@@ -56,6 +59,7 @@ $statement->execute();
 $id = $dbh->lastInsertId();
 echo "<p>Pet $id inserted successfully.</p>";
 
+
 //Bind the parameters
 $type = 'cat';
 $name = 'kotik';
@@ -68,6 +72,9 @@ $statement->bindParam(':color', $color, PDO::PARAM_STR);
 $statement->execute();
 $id = $dbh->lastInsertId();
 echo "<p>Pet $id inserted successfully.</p>";
+
+
+//UPDATE QUERY
 
 
 //Define the query
@@ -86,6 +93,8 @@ $statement->bindParam(':new', $new, PDO::PARAM_STR);
 $statement->execute();
 
 
+//UPDATE QUERY
+
 
 //Define the query
 $sql = "UPDATE pets SET name = :new WHERE name = :old";
@@ -103,6 +112,8 @@ $statement->bindParam(':new', $new, PDO::PARAM_STR);
 $statement->execute();
 
 
+//UPDATE QUERY
+
 
 //Define the query
 $sql = "UPDATE pets SET color = :new WHERE color = :old";
@@ -119,3 +130,40 @@ $statement->bindParam(':new', $new, PDO::PARAM_STR);
 //Execute
 $statement->execute();
 
+
+//DELETE QUERY
+
+
+//Define the query
+$sql = "DELETE FROM pets WHERE id = :id";
+
+//Prepare the statement
+$statement = $dbh->prepare($sql);
+
+//Bind the parameters
+$id = 1;
+$statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+//Execute
+$statement->execute();
+
+
+//SELECT QUERY
+
+
+//Define the query
+$sql = "SELECT * FROM pets WHERE id = :id";
+
+//Prepare the statement
+$statement = $dbh->prepare($sql);
+
+//Bind the parameters
+$id = 3;
+$statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+//Execute the statement
+$statement->execute();
+
+//Process the result
+$row = $statement->fetch(PDO::FETCH_ASSOC);
+echo $row['name'] . ", " . $row['type'] . ", " . $row['color'];
